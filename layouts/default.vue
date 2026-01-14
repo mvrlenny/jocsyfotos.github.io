ffc161f2402ab3abf0ff0e4a0b5ed236f4b01dee
Marlenny Richardson <mvrlenny@gmail.com>
2026-01-14 14:22:03 -0400

    fix absolute image paths for static deploy

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
     <Container class="relative">
       <div class="absolute top-0 right-0">
         <NuxtImg
-          src="img/bg-glow.png"
+          src="/img/bg-glow.png"
           aria-hidden="true"
           class="w-[44rem]"
           format="webp"
--- layouts/story.vue
+++ layouts/story.vue
@@ -7,7 +7,7 @@
     </div>
     <Container class="relative">
       <div class="absolute top-0 right-0">
-        <NuxtImg src="img/bg-glow.png" aria-hidden="true" class="w-[44rem]" format="webp" width="944" height="586" />
+        <NuxtImg src="/img/bg-glow.png" aria-hidden="true" class="w-[44rem]" format="webp" width="944" height="586" />
       </div>
     </Container>
     <div class="relative">
