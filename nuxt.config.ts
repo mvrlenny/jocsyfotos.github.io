870e8bc19b7df1c354ae8658f3e839deb6934214
Marlenny Richardson <mvrlenny@gmail.com>
2026-04-18 23:18:09 -0400

    Add Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,23 +42,38 @@ image: {
    * <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
    */
   app: {
-    head: {
-      link: [
-        {
-          rel: "preconnect",
-          href: "https://fonts.googleapis.com",
-        },
-        {
-          rel: "preconnect",
-          href: "https://fonts.gstatic.com",
-        },
-        {
-          href: "https://fonts.googleapis.com/css2?Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,600;0,700;1,400&display=swap",
-          rel: "stylesheet",
-        },
-      ],
-    },
+  head: {
+    link: [
+      {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+      },
+      {
+        href: "https://fonts.googleapis.com/css2?Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,600;0,700;1,400&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+
+    script: [
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-34SYEXBN1S",
+        async: true,
+      },
+      {
+        children: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-34SYEXBN1S');
+        `,
+      },
+    ],
   },
+},
 
   content: {
     // https://content.nuxtjs.org/api/configuration
